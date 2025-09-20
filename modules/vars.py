31363cbc988391874f1a8fbe72623772c6d368bc
shivanggupta1221-jpg <shivanggupta1221@gmail.com>
2025-09-20 14:26:32 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -20,9 +20,10 @@ if int(OWNER) not in AUTH_USERS:
     AUTH_USERS.append(int(OWNER))
   
 # .....,.....,.......,...,.......,....., .....,.....,.......,...,.......,.....,
-api_url = "http://master-api-v3.vercel.app/"
-api_token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiNzkxOTMzNDE5NSIsInRnX3VzZXJuYW1lIjoi4p61IFtvZmZsaW5lXSIsImlhdCI6MTczODY5MjA3N30.SXzZ1MZcvMp5sGESj0hBKSghhxJ3k1GTWoBUbivUe1I"
+api_url = "http://aster-api-v3.vercel.app/"
+api_token = "yJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiNzkxOTMzNDE5NSIsInRnX3VzZXJuYW1lIjoi4p61IFtvZmZsaW5lXSIsImlhdCI6MTczODY5MjA3N30.SXzZ1MZcvMp5sGESj0hBKSghhxJ3k1GTWoBUbivUe1I"
 # .....,.....,.......,...,.......,....., .....,.....,.......,...,.
+
 
 
 
